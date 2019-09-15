00564959bca9d0c33e7e07ad6bd2d12d25395189
SergejDK <35216985+SergejDK@users.noreply.github.com>
2019-09-15 17:45:03 +0200

    Create updateXFVersion.sh

---
+++ Fabulous/Fabulous.XamarinForms/updateXFVersion.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+TEMPLATE_FILE="Fabulous.XamarinForms/templates/content/blank/.template.config/template.json"
+PACKAGE_TARGETS="Packages.targets"
+PAKET_DEPENDENCIES="paket.dependencies"
+PAKET_LOCK="paket.lock"
+
+FILES_TO_CHANGE=( $TEMPLATE_FILE $PACKAGE_TARGETS $PAKET_DEPENDENCIES $PAKET_LOCK )
+        
+if [ $# -ne 2 ]
+then
+        echo "please input a versionnumber"
+        exit 1
+fi
+
+OLD_XF_VERSION=$1
+NEW_XF_VERSION=$2
+
+for versionFile in ${FILES_TO_CHANGE[@]}
+do
+        sed -i '' "s/$OLD_XF_VERSION/$NEW_XF_VERSION/g" $versionFile
+        echo "File changed: $versionFile"
+done
+
+echo "Successful change to version $NEW_XF_VERSION"
+exit 0
